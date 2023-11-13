8084c6679f8fcc7355f9d5dfd25c139e066f6fd9
Ramir Vorobyev <rrramirrr@yandex.ru>
2023-11-13 10:32:45 +0300

    Update 5723.py

--- 5 ЕГЭ/5723.py
+++ 5 ЕГЭ/5723.py
@@ -2,14 +2,14 @@ count_R = 0
 
 for N in range(1, 1000):
     N_bin = bin(N)[2:]
+    
     if N_bin.count('1') % 2 == 0:
         N_bin = "10" + N_bin + "11"
     else:
         N_bin = "11" + N_bin + "11"
     
     R = int(N_bin, 2)
-    
     if R < 860:
         count_R += 1
-        
+            
 print(count_R)
